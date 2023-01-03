70ad79a930e4be533d66113aa405787ebdc7b1fa
rui-watanabe <wataru610806@gmail.com>
2023-01-03 16:16:02 +0900

    getPhotoByIdのImpl作成

--- app/src/main/java/com/example/unsplashclient/data/repository/PhotoRepositoryImpl.kt
+++ app/src/main/java/com/example/unsplashclient/data/repository/PhotoRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.unsplashclient.data.repository
 
+import com.example.unsplashclient.data.remote.PhotoDetailsDto
 import com.example.unsplashclient.data.remote.SearchPhotosResultDto
 import com.example.unsplashclient.data.remote.UnsplashApi
 import com.example.unsplashclient.domain.repository.PhotoRepository
@@ -10,5 +11,9 @@ class PhotoRepositoryImpl @Inject constructor(
     ) : PhotoRepository {
     override suspend fun searchPhotos(query: String): SearchPhotosResultDto {
         return api.searchPhotos(query)
+    }
+
+    override suspend fun getPhotoById(id: String): PhotoDetailsDto {
+        return api.getPhotoById(id)
     }
 }
